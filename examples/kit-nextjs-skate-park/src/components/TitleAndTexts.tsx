0ddab4687a0df58bbc25c691b6c105df0223790d
Manduru, Abjal <abjal.manduru@wattswater.com>
2025-11-04 15:36:51 -0500

    using servsideprops

--- examples/kit-nextjs-skate-park/src/components/TitleAndTexts.tsx
+++ examples/kit-nextjs-skate-park/src/components/TitleAndTexts.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Field, Text } from '@sitecore-content-sdk/nextjs';
-import useSWR from 'swr';
+
 interface Fields {
   Title: Field<string>;
   Text: Field<string>;
@@ -16,8 +16,6 @@ type TitleAndTextProps = {
 export const Default = (props: TitleAndTextProps): React.ReactElement => {
   const containerStyles = props.params && props.params.styles ? props.params.styles : '';
   const styles = `${props.params.GridParameters} ${containerStyles}`.trimEnd();
-  const {isLoading,data}=useSWR(
-    'https://ods-prod.watts.com/restapi/api/watts/ods/products/8f0b8be1-e8d3-4c66-8a1b-115ba3083436',{fallbackData:props.product});
 
   return (
     <div className={`container-default component ${styles}`}>
@@ -33,23 +31,19 @@ export const Default = (props: TitleAndTextProps): React.ReactElement => {
       <div className="component-content text row">
         <Text field={props.fields.Text1} />
       </div>
-     <div><span>Short Description</span>{isLoading?'Loading..':data.Versions[0].ShortDescription}</div>
-     <div><span>Long Description</span>{isLoading?'Loading..':data.Versions[0].LongDescription}</div>
+      {props.product && (
+        
+        <div className="component-content product-data row">
+         <h1>{props.product.ShortDescription}</h1>
+          <h2>{props.product.LongDescription}</h2>
+        </div>
+      )}
     </div>
   );
 };
 
-export const getServerSideProps = async (_rendering, layoutData) => {
-  const isEditing = layoutData.sitecore.context.pageEditing;
-  if(!isEditing) {
-    return { props: {} };
-  }
-  return{
-    props: {
-      product: "Sample Product Short Description",
-    }
-  }
- /*  console.log("https://ods-prod.watts.com/restapi/api/watts/ods/products/8f0b8be1-e8d3-4c66-8a1b-115ba3083436");
+export const getServerSideProps = async () => {
+  console.log("https://ods-prod.watts.com/restapi/api/watts/ods/products/8f0b8be1-e8d3-4c66-8a1b-115ba3083436");
   const pimData = await fetch("https://ods-prod.watts.com/restapi/api/watts/ods/products/8f0b8be1-e8d3-4c66-8a1b-115ba3083436");
   const data = await pimData.json();
   console.log("PIM Data:", data);
@@ -57,5 +51,5 @@ export const getServerSideProps = async (_rendering, layoutData) => {
     props: {
       product: data?.Versions[0]
     }
-  }; */
+  };
 };
